e72a1d32a289758284b59e54f75c25c7a21867da
Brian Rodham <brian.j.rodham@gmail.com>
2017-07-28 00:07:27 -0600

    Assignment 4 - Added Animation and multiple puzzles

--- js/controller.js
+++ js/controller.js
@@ -1,40 +1,47 @@
 
 //var game = new gameData(); // Model
+var selectedCell = null;
 
-function validate(){
-   game.validate();
+function validate() {
+    game.validate();
 }
 
-function generateBoard(rows, columns){
+function generateBoard(rows, columns) {
     generateTable(rows, columns);
-    populateData();
+    newGame(DIFFICULTY.EASY);
+    startAnimation();
 }
 
-function generateTable(rows, columns){
+
+
+function generateTable(rows, columns) {
     var gameContainer = document.getElementById('game-container');
 
     // Create the container for the grid
     var gameTable = document.createElement("table");
-    gameTable.setAttribute("id","game-table");
+    gameTable.setAttribute("id", "game-table");
     gameContainer.appendChild(gameTable);
 
-    for(i = 1; i <= rows; i++){
+    for (i = 1; i <= rows; i++) {
         var row = document.createElement("tr"); // create the row
 
         // Add a black border to the bottom of the cells of every third row
-        if(i && (i % 3 === 0)){
+        if (i && (i % 3 === 0)) {
             row.className = "third-row";
         }
 
-        for(j = 1; j <= columns; j++){
+        for (j = 1; j <= columns; j++) {
             var cell = document.createElement("td");
-
+            var input = document.createElement("input");
+            input.type = "text";
+            input.maxLength=1;
+            input.onkeypress = function(event) {return event.charCode >= 49 && event.charCode <= 57}
+            cell.appendChild(input);
             /* Cell styling using javascript */
-            if(j==1){
+            if (j == 1) {
                 cell.className = "first-cell";
             }
-            else if(j && (j % 3 === 0))
-            {
+            else if (j && (j % 3 === 0)) {
                 cell.className = "third-cell";
             }
 
@@ -44,26 +51,106 @@ function generateTable(rows, columns){
     }
 }
 
+
 // Takes the data from the model and puts it in the view
-function populateData(){
+function populateData() {
+    console.log("Populating grid");
+    console.log(game.board);
     var value;
     var cellValue;
-    var grid = game.board; // Gets the grid data from the model
+    //var grid = game.board; // Gets the grid data from the model
     var table = document.getElementById('game-table');
-    for (var i = 0, row; row = table.rows[i]; i++) { //iterate through rows      
-        for (var j = 0, col; col = row.cells[j]; j++) { //iterate through columns
-            value = game.getValue(i,j);
-            if(value.includes(":pre")){ // If the value is a preset default
-                row.cells[j].classList.add("pre");
-                cellValue = value.replace(":pre","");
-                row.cells[j].innerHTML = cellValue ;
-            }
-            else { // If the value is user defined (Probably won't be used unless showing game in progress)
-                row.cells[j].innerHTML = value;
-            }
-        }  
-    }
+    for (var i = 0, row; row = table.rows[i]; i++) { //iterate through rows     
 
+        for (var j = 0, col; col = row.cells[j]; j++) { //iterate through columns
+            (function (i, j, row, col) {
+                value = game.getValue(i, j);
+               // console.log("Adding data " + value + " to cell: " + i.toString() + "," + j.toString());
+
+                if (value.includes(":pre")) { // If the value is a preset default
+                    row.cells[j].classList.add("pre");
+                    cellValue = value.replace(":pre", "");
+                    row.cells[j].firstChild.value = cellValue;
+                    row.cells[j].firstChild.readOnly = true;
+                }
+                else { // Clears out the old styling
+                    row.cells[j].firstChild.value = value;
+                    row.cells[j].firstChild.readOnly = false;
+                    row.cells[j].classList.remove("pre");
+                   
+                }
+                row.cells[j].onclick = function () {
+                    if(selectedCell != null){
+                        selectedCell.classList.remove("selected");
+                    }
+                    document.getElementById("itemClicked").innerHTML = "Cell clicked: " + i + ',' + j;
+                    row.cells[j].classList.add("selected");
+                    selectedCell = row.cells[j];
+                }
+            })(i, j, row, col)
+        }
+    }
 }
 
-generateBoard(9,9);
+function reset() {
+    game.board.forEach(function (row, i) {
+        row.forEach(function (cell, j) {
+            console.log(cell);
+            if (!cell.includes(":pre")) {
+                //cell = "";
+                game.board[i][j] = "";
+            }
+        });
+    });
+    populateData();
+}
+
+
+function newGame(difficulty){
+    console.log("NEW GAME");
+    var difficulty = difficulty;
+    if(difficulty==undefined){
+       var input = document.getElementById("newGame").value;
+       difficulty = DIFFICULTY[input];
+    }
+    console.log("Loading game with difficulty " + difficulty);
+    game.loadBoard(difficulty, populateData);
+    
+}
+
+
+function showModelData(){
+    document.getElementById("modelData").classList.remove("hidden");
+}
+function hideModelData(){
+    document.getElementById("modelData").classList.add("hidden");
+}
+
+function startAnimation(){
+    console.log("ANIMATING");
+    var element = document.getElementById("gameTitle");
+    var speed = 16;
+    element.style.top = "-500px";
+    setTimeout(moveElementDown, speed);
+}
+function moveElementDown(){
+    var speed = 16;
+    var element = document.getElementById("gameTitle");
+    var currentTop  =  element.style.top.substring(0,element.style.top.length-2);
+    var newTop = parseInt(currentTop) + 12;
+    element.style.top =  newTop + "px";
+    if(newTop < 10){
+     setTimeout(moveElementDown, speed);
+    }
+}
+
+var sec = 0;
+function pad ( val ) { return val > 9 ? val : "0" + val; }
+setInterval( function(){
+    document.getElementById("seconds").innerHTML=pad(++sec%60);
+    document.getElementById("minutes").innerHTML=pad(parseInt(sec/60,10));
+}, 1000);
+
+
+generateBoard(9, 9);
+console.log("Starting?");
--- js/model.js
+++ js/model.js
@@ -1,19 +1,67 @@
+
 var game = {
 
 
     board:  [
-                    ['1:pre', '7:pre', '', '5:pre', '2:pre', '', '', '', ''],
-                    ['', '2', '8', '', '7:pre', '9:pre','', '6:pre', '3:pre'],
-                    ['', '', '9:pre','8:pre', '3:pre', '4:pre','7:pre', '', '1:pre'],
-                    ['', '', '','4:pre', '', '','', '3:pre', '2:pre'],
-                    ['', '4:pre', '','', '5', '2','', '', '6:pre'],
-                    ['7:pre', '', '','3:pre', '1:pre', '','', '', '4:pre'],
-                    ['5:pre', '', '','', '8', '','', '4:pre', '9:pre'],
-                    ['', '8:pre', '','', '9:pre', '','', '', ''],
-                    ['9:pre', '1:pre', '','', '4:pre', '5:pre','3:pre', '8', '7']     
+                    ['', '', '', '', '', '', '', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', '']     
+    ],
+    key:  [
+                    ['', '', '', '', '', '', '', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', ''],
+                    ['', '', '', '', '', '','', '', '']     
     ],
     gameOver: false,
     gameTime: "00:05:25",
+    //Puzzles
+    easyPuzzles: ['easy-1.json','easy-2.json','easy-3.json', 'easy-4.json'],
+    mediumPuzzles: ['medium-1.json','medium-2.json','medium-3.json'],
+    hardPuzzles: ['hard-1.json','hard-2.json','hard-3.json'],
+    loadBoard: function(difficulty, callback){
+
+        var path = "puzzles/";
+        if(difficulty == DIFFICULTY.EASY){
+            path = path + this.easyPuzzles[Math.floor(Math.random() * this.easyPuzzles.length)];
+        }
+        else if(difficulty == DIFFICULTY.MEDIUM) {
+            path = path + this.mediumPuzzles[Math.floor(Math.random() * this.mediumPuzzles.length)];
+        }
+        else if(difficulty == DIFFICULTY.HARD){
+            path = path + this.hardPuzzles[Math.floor(Math.random() * this.hardPuzzles.length)];
+        }
+        else {
+            throw("An invalid difficulty level was entered.");
+        }
+
+        var httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4) {
+                if (httpRequest.status === 200) {
+                    var gameData = JSON.parse(httpRequest.responseText);
+                    game.board = gameData.start;
+                    game.key = gameData.key;
+                    console.log(game.key);
+                    callback();
+                }
+            }
+        };
+        httpRequest.open('GET', path, false);
+        httpRequest.send(); 
+
+    },
     // Sets the value of specific box in the model
     setValue: function(value, row, col) {
         this.board[row][col] = value;
@@ -25,15 +73,22 @@ var game = {
 
     // Validates the game data to see if we've won or not
     validate: function(){
-        if( this.checkRows() && this.checkCols() && this.checkSquares()){
-            //this.gameOver = true;
-            console.log("Validating data");
-        }
-    },
+        console.log("Validating data");
+        /*for(var i = 0; row = table.row[i]; i++){
+            for( var j=0; col = rows.cells[j]; j++){
+                
+            }
+        }*/
+        /*if( this.checkRows() && this.checkCols() && this.checkSquares()){
+            this.gameOver = true;
+        }*/
+    }
 
     // Checks to see if all of the rows are correct
-    checkRows: function(){
-        return true;
+    /*checkRows: function(){
+        this.board.forEach(function(element){
+            
+        });
     },
 
     // Checks to see if all the cols are correct
@@ -44,5 +99,11 @@ var game = {
     // Checks to see if the 3x3 squares are all correct
     checkSquares: function(){
         return true;
-    }
+    }*/
 }
+
+var DIFFICULTY = {
+  EASY : {value: 0, name: "Easy", code: "E"}, 
+  MEDIUM: {value: 1, name: "Medium", code: "M"}, 
+  HARD : {value: 2, name: "Hard", code: "H"}
+};
